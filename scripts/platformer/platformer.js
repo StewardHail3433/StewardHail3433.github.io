40cf5b39ef8969624672f613a93da8e5017ca295
Ethan Porter <emanporter2009@gmail.com>
2024-05-14 19:17:57 -0400

    updated info

--- scripts/platformer/platformer.js
+++ scripts/platformer/platformer.js
@@ -6,8 +6,11 @@ const runButton = document.getElementById("runButton");
 if(screen.availWidth>screen.availHeight) {
     ctx.canvas.width = screen.availWidth*.7;
     ctx.canvas.height = screen.availHeight*.7;
+    document.getElementById("test").innerHTML = "<p>" +screen.availWidth + ", " + screen.availHeight + "<br>"+ctx.canvas.width + ", " + ctx.canvas.height +"</p>" ;
 } else {
-    ctx.canvas.width = screen.availWidth;
-    ctx.canvas.height = screen.availHeight;}
-document.getElementById("test").innerHTML = "<p>" +screen.availWidth + ", " + screen.availHeight + "<br>"+ctx.canvas.width + ", " + ctx.canvas.height +"</p>" ;
+    ctx.canvas.width = screen.width;
+    ctx.canvas.height = screen.height;
+    document.getElementById("test").innerHTML = "<p>" +screen.availWidth + ", " + screen.availHeight + "<br>"+screen.width + ", " + screen.height +"</p>" ;
+}
+
 
