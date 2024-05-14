3d00b03098055c0dd7a74d8205591e8637806156
Ethan Porter <emanporter2009@gmail.com>
2024-05-14 19:06:12 -0400

    see screen info

--- scripts/platformer/platformer.js
+++ scripts/platformer/platformer.js
@@ -7,6 +7,7 @@ if(screen.availWidth>screen.availHeight) {
     ctx.canvas.width = screen.availWidth*.7;
     ctx.canvas.height = screen.availHeight*.7;
 } else {
-    ctx.canvas.width = screen.availWidth*7;
-    ctx.canvas.height = screen.availHeight*7;}
+    ctx.canvas.width = screen.availWidth;
+    ctx.canvas.height = screen.availHeight;}
+document.getElementById("test").innerHTML = "<p>" +screen.availWidth + ", " + screen.availHeight + "<br>"+ctx.canvas.width + ", " + ctx.canvas.height +"</p>" ;
 
