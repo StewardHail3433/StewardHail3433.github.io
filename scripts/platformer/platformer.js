442b6e8838532aacc84f11d32038ff29d9f7c269
Ethan Porter <emanporter2009@gmail.com>
2024-05-14 19:00:20 -0400

    test

--- scripts/platformer/platformer.js
+++ scripts/platformer/platformer.js
@@ -3,5 +3,10 @@ const canvas = document.getElementById("platformerCanvas");
 const ctx = canvas.getContext("2d");
 const runButton = document.getElementById("runButton");
 
-ctx.canvas.width = screen.availWidth*.7;
-ctx.canvas.height = screen.availHeight*.7;
+if(screen.availWidth>screen.availHeight) {
+    ctx.canvas.width = screen.availWidth*.7;
+    ctx.canvas.height = screen.availHeight*.7;
+} else {
+    ctx.canvas.width = screen.availWidth*7;
+    ctx.canvas.height = screen.availHeight*7;}
+
