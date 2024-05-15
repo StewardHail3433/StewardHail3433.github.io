311383b0e7365d32ee6f0f7e40a55c0e7156de11
Ethan Porter <emanporter2009@gmail.com>
2024-05-14 21:29:32 -0400

    add canvas para

--- scripts/platformer/platformer.js
+++ scripts/platformer/platformer.js
@@ -9,9 +9,9 @@ let interval;
 
 var button = new Button(canvas, 10, 10, 40, 40, "red");
 
-if(screen.availWidth>screen.availHeight) {
-    ctx.canvas.width = screen.availWidth*.8;
-    ctx.canvas.height = screen.availHeight*.7;
+if (screen.availWidth > screen.availHeight) {
+    ctx.canvas.width = screen.availWidth * .8;
+    ctx.canvas.height = screen.availHeight * .7;
     //document.getElementById("test").innerHTML = "<p>" +screen.availWidth + ", " + screen.availHeight + "<br>"+ctx.canvas.width + ", " + ctx.canvas.height +"</p>" ;
 } else {
     ctx.canvas.width = screen.width;
@@ -21,10 +21,10 @@ if(screen.availWidth>screen.availHeight) {
 
 function isTouchDevice() {
     return (('ontouchstart' in window) ||
-       (navigator.maxTouchPoints > 0) ||
-       (navigator.msMaxTouchPoints > 0));
-  }
-  
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0));
+}
+
 document.getElementById("test").innerHTML = "<p>" + isTouchDevice() + "</p>";
 button.test();
 function update() {
@@ -39,13 +39,13 @@ function touchMove(e) {
     e.p
 }
 runButton.addEventListener("click", function () {
-    canvas.addEventListener("touchend", function (event) {button.touchButton(event.touches[0]);document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " start"}, {passive:false});
-    canvas.addEventListener("touchmove", function (event) {button.touchButton(event.touches[0]); event.preventDefault(); document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " mid"}, {passive:false});
-    canvas.addEventListener("touchstart", function (event) {button.touchButton(event.touches[0]); document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " end"}, {passive:false});
+    canvas.addEventListener("touchend", function (event) { button.touchButton(event.touches[0], canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " start" }, { passive: false });
+    canvas.addEventListener("touchmove", function (event) { button.touchButton(event.touches[0], canvas); event.preventDefault(); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " mid" }, { passive: false });
+    canvas.addEventListener("touchstart", function (event) { button.touchButton(event.touches[0], canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " end" }, { passive: false });
 
-    canvas.addEventListener("mousedown", function (event) {button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " start"}, {passive:false});
-    canvas.addEventListener("mousemove", function (event) {button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " mid"}, {passive:false});
-    canvas.addEventListener("mouseend", function (event) {button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText  + " end"}, {passive:false});
+    canvas.addEventListener("mousedown", function (event) { button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " start" }, { passive: false });
+    canvas.addEventListener("mousemove", function (event) { button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " mid" }, { passive: false });
+    canvas.addEventListener("mouseend", function (event) { button.touchButton(event, canvas); document.getElementById("test2").innerText = document.getElementById("test2").innerText + " end" }, { passive: false });
 
     interval = setInterval(update, 10);
     this.disable = false;
