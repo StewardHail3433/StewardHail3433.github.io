3816ac5fa3ac353faed275ed304b649d34e7bd02
Ethan Porter <emanporter2009@gmail.com>
2024-05-22 19:31:45 -0400

    map scale added

--- scripts/platformer2/map/map.js
+++ scripts/platformer2/map/map.js
@@ -8,7 +8,8 @@ export default class Map {
     #xVel = 0.25;
     constructor(/** @type {CanvasRenderingContext2D} */ ctx) {
         this.ctx = ctx;
-        this.tileSize = this.ctx.canvas.height/16.4375;
+        this.tileSize = Math.floor(ctx.canvas.height / 16.44);
+        this.yOffset = Math.floor(this.tileSize);
         fetch("./scripts/platformer2/resources/map.txt")
             .then((res) => res.text())
             .then((text) => {
@@ -16,13 +17,15 @@ export default class Map {
             /** @type {String[]} */ let lines = text.trim().split('\n');
                 for (let i = 0; i < lines.length; i++) {
                     this.map[i] = [];
+                    let startingY = -(lines.length * this.tileSize) + ctx.canvas.height - this.tileSize
                     for (let j = 0; j < lines[i].split(" ").length; j++) {
                         this.map[i][j] = new Tile(this.ctx);
                         this.map[i][j].value = lines[i].split(" ")[j];
                         this.map[i][j].x = j * this.tileSize;
-                        this.map[i][j].y = i * this.tileSize + this.tileSize/2;
+                        this.map[i][j].y =  (startingY + (i + 1) * this.tileSize);
                         this.map[i][j].width = this.tileSize;
                         this.map[i][j].height = this.tileSize;
+                        console.log(this.map[i][j])
                     }
                 }
             })
