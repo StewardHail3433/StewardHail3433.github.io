f4772a14bf31d011660a2225f6220b58a1e0c382
Ethan Porter <115738405+StewardHail3433@users.noreply.github.com>
2025-04-08 12:34:40 -0400

    hi

--- scripts/typePlatformer/utils/Constants.ts
+++ scripts/typePlatformer/utils/Constants.ts
@@ -4,7 +4,7 @@ export class Constants {
     public static readonly CANVAS_WIDTH = 480;
     public static readonly CANVAS_HEIGHT = 320;
     public static readonly TILE_SIZE = 16;
-    public static readonly WORLD_WIDTH = 500;
-    public static readonly WORLD_HEIGHT = 500;
+    public static readonly WORLD_WIDTH = 50;
+    public static readonly WORLD_HEIGHT = 50;
     public static readonly COMMAND_SYSTEM = new CommandSystem();
 }
