b1d94d46e246568f51b81c9f549904dcd98f2efb
StewardHail3433 <115738405+StewardHail3433@users.noreply.github.com>
2025-04-08 16:19:29 +0000

    Deploying to main from @ StewardHail3433/StewardHail3433.github.io@34f2505c45e9ab7d508fb68a5b58d1fa06045c8e 🚀

--- dist/typePlatformer/utils/Constants.js
+++ dist/typePlatformer/utils/Constants.js
@@ -4,6 +4,6 @@ export class Constants {
 Constants.CANVAS_WIDTH = 480;
 Constants.CANVAS_HEIGHT = 320;
 Constants.TILE_SIZE = 16;
-Constants.WORLD_WIDTH = 50;
-Constants.WORLD_HEIGHT = 50;
+Constants.WORLD_WIDTH = 5000;
+Constants.WORLD_HEIGHT = 5000;
 Constants.COMMAND_SYSTEM = new CommandSystem();
