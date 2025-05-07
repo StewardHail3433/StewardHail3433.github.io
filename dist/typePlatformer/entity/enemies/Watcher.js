6ff788e007e0001ecfa8533175a684d7a4712d14
StewardHail3433 <115738405+StewardHail3433@users.noreply.github.com>
2025-05-07 16:54:31 +0000

    Deploying to main from @ StewardHail3433/StewardHail3433.github.io@1e755c105dc59f30fab249758a30164fdd2d1eab 🚀

--- dist/typePlatformer/entity/enemies/Watcher.js
+++ dist/typePlatformer/entity/enemies/Watcher.js
@@ -5,25 +5,30 @@ export class Watcher extends Entity {
         this.lockActionTime = 0;
     }
     update() {
-        this.velocity = { x: 0, y: 0 };
         if (this.lockActionTime > 200) {
             const random1 = Math.random() * 100;
             const random2 = Math.random() * 100;
-            if (random1 > 49) {
+            if (random1 < 33) {
                 this.direction = "up";
                 this.velocity.y = -this.speed;
             }
-            else {
+            else if (random1 < 66) {
                 this.direction = "down";
                 this.velocity.y = this.speed;
             }
-            if (random2 > 49) {
+            else {
+                this.velocity.y = 0;
+            }
+            if (random2 < 33) {
                 this.direction = "left";
                 this.velocity.x = -this.speed;
             }
-            else {
+            else if (random2 < 66) {
                 this.direction = "right";
                 this.velocity.x = this.speed;
+            }
+            else {
+                this.velocity.x = 0;
             }
             this.lockActionTime = 0;
         }
--- dist/typePlatformer/script.js
+++ dist/typePlatformer/script.js
@@ -239,9 +239,9 @@ class Game {
             this.worldHandler.renderLayer(i, this.ctx, this.camera);
         }
         this.worldHandler.renderDropItems(this.ctx, this.camera);
-        // for(let i = 0; i < this.enemies.length; i++) {
-        //     this.enemies[i].render(this.ctx);
-        // }
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.enemies[i].render(this.ctx);
+        }
         this.player.render(this.ctx);
         for (let i = this.player.getLayer() + 1; i < 2; i++) {
             this.worldHandler.renderLayer(i, this.ctx, this.camera);
