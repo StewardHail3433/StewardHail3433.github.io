820634ddd61e8efc413fb473796effc74203525d
StewardHail3433 <115738405+StewardHail3433@users.noreply.github.com>
2025-05-15 18:01:42 +0000

    Deploying to main from @ StewardHail3433/StewardHail3433.github.io@d52dd87b5cddb434a1c53fc9ada82b9d9ff62c33 🚀

--- dist/typePlatformer/ui/UIHandler.js
+++ dist/typePlatformer/ui/UIHandler.js
@@ -94,6 +94,10 @@ export class UIHandler {
         this.characterChooserLeftButton.setParentComponent(this.characterChooserComponent);
         this.characterChooserRightButton.setParentComponent(this.characterChooserComponent);
         Constants.COMMAND_SYSTEM.addCommand("debug", (args) => {
+            if (args.length == 0) {
+                Constants.INPUT_HANDLER.setToggle("F3", !Constants.INPUT_HANDLER.getKeyToggled());
+                return;
+            }
             if (args[0] === "show") {
                 Constants.INPUT_HANDLER.setToggle("F3", true);
             }
