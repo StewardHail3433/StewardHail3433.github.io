14bc5317260f2250aa6fc1c33e7b2ebf5f16e1ab
StewardHail3433 <115738405+StewardHail3433@users.noreply.github.com>
2025-05-16 03:19:28 +0000

    Deploying to gh-pages from @ StewardHail3433/StewardHail3433.github.io@d6cd192df47c5684eaf1dedc05c6119df0c4b59b 🚀

---
+++ scripts/entity.js
@@ -0,0 +1,57 @@
+export default class Entity {
+    constructor(x, y, width = 50, height = 50, speed = 0, gravity = 0.5, /** @type {CanvasRenderingContext2D} */ ctx) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.vx = 0; 
+        this.vy = 0; 
+        this.gravity = gravity; 
+        this.grounded = false;
+        this.movingLeft = false;
+        this.movingRight = false;
+        this.isJumping = false;
+        /** @type {CanvasRenderingContext2D} */ this.ctx = ctx;
+    }
+
+    update(deltaTime) {
+        if(this.movingLeft && this.movingRight){
+            this.vx = 0
+        } else{
+            if(!this.movingLeft && !this.movingRight){
+                this.vx = 0
+            } else{
+                if(this.movingLeft && !this.movingRight) {
+                    this.vx = -this.speed;
+                } 
+                if(this.movingRight && !this.movingLeft) {
+                    this.vx = this.speed;
+                }
+            }
+            
+        }
+        if (!this.grounded) {
+            this.vy += this.gravity;
+        } 
+        
+
+        this.x += this.vx * deltaTime;
+        this.y += this.vy * deltaTime;
+
+        if (this.y + this.height >= this.ctx.canvas.height) {
+            this.y = this.ctx.canvas.height - this.height;
+            this.vy = 0;
+            this.grounded = true;
+        } else {
+            //this.grounded = false;
+        }
+        
+    }
+
+    render() {
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+}
---
+++ scripts/main.js
@@ -0,0 +1,189 @@
+import UI from "./UI/ui.js";
+import Player from "./entity/player.js";
+import Map from "./map/map.js";
+import Camera from "./camera/camera.js";
+import { CONSTANTS } from "./utils/gameConst.js";
+import SimpleEnemy from "./entity/enemy/simpleEnemy.js";
+import ShooterEnemy from "./entity/enemy/shooterEnemy.js";
+import CanonEnemy from "./entity/enemy/CanonEnemy.js";
+import Sound from "./sound/sound.js";
+
+const canvas = document.getElementById('gameCanvas');
+/** @type {CanvasRenderingContext2D} */ const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false;
+
+ctx.canvas.width = screen.availWidth * .8;
+ctx.canvas.height = screen.availHeight * .7;
+
+var sound = new Sound();
+
+ctx.canvas.style = "background-color: rgb(225, 171, 84);"
+
+var state = "play";
+
+var camera = new Camera(ctx);
+var map = new Map(ctx, camera);
+
+var player = new Player(ctx, map.map);
+
+var enemies = []
+for(let i = 0; i < 10; i++) {
+    enemies.push(new SimpleEnemy(ctx, map.map,camera, player, sound, i))
+}
+
+enemies.push(new CanonEnemy(ctx, map.map,camera,player,{x: 1237, y:470}, sound, enemies.length))
+enemies.push(new ShooterEnemy(ctx, map.map,camera,player,{x: 1237, y:470}, sound, enemies.length))
+
+var ui = new UI(ctx, player, camera, enemies, state, sound);
+// camera.setTarget(map.map[-1][0]);
+camera.setTarget(player);
+
+
+const FPS = 60;
+const perfectFrameTime = 1000 / FPS;
+let elapsedTime = 0; 
+let lastTimestamp = 0;
+
+
+function gameLoop(timestamp) {
+    const deltaTime = (timestamp - lastTimestamp) / 1000; // deltaTime in seconds
+    lastTimestamp = timestamp;
+    
+        update(deltaTime);
+        render();
+    
+
+    requestAnimationFrame(gameLoop);
+
+}
+
+function update(deltaTime) {
+    if(state == "play") {
+        if (player.alive ) {
+            elapsedTime += deltaTime;
+            for(let i = 0; i < enemies.length; i++) {
+                enemies[i].update(deltaTime, elapsedTime);
+                if(enemies[i].shouldDelete) {enemies.splice(i, 1);}
+            }
+            player.update(deltaTime, enemies);  
+
+            map.update();
+            camera.update();
+        }
+    }
+    ui.update(state);
+
+    if (!player.alive || state == "pause") { 
+        sound.stop("music")
+    } else {
+        if(0 != sound.getCurrentMusic()) {
+            sound.play("music", 0);
+        }
+    }
+}
+
+function render() {
+    
+    
+    //map.renderMini()
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();  
+
+    ctx.scale(CONSTANTS.canvasScale, CONSTANTS.canvasScale);
+    ctx.translate(Math.round(-camera.pos.x), Math.round(-camera.pos.y));
+
+    ctx.clearRect(0, 0, canvas.width / CONSTANTS.canvasScale, canvas.height / CONSTANTS.canvasScale);
+    map.render();
+    for(let i = 0; i < enemies.length; i++) {
+    enemies[i].render();}
+    player.render();
+     //-camera.height/2)
+    // splayer.render();
+    ctx.restore();
+    
+    
+
+    ui.render();
+    renderTimer();
+}
+
+function renderTimer() {
+    const seconds = Math.floor(elapsedTime);
+
+    ctx.save();
+    ctx.fillStyle = 'white';
+    ctx.font = '20px Arial';
+    ctx.fillText("Time: " + seconds.toString().padStart(2, '0'), 10, 30);
+    ctx.restore();
+}
+
+
+document.getElementById("runButton").addEventListener("click", function () {
+    lastTimestamp = performance.now();
+    sound.play("music", 0);
+    player.pos = {
+        x: 0,//ctx.canvas.width/2 - this.width/2,
+        y: 0//ctx.canvas.height/2 - this.height/2
+    }
+
+    requestAnimationFrame(gameLoop);
+    this.disabled = true;
+})
+
+canvas.addEventListener('keydown', function(e) {
+    e.preventDefault()
+    if (e.key === "p") {
+        camera.setTarget(player);
+    };
+    if (e.key === "b") {
+        camera.setTarget(map.map[199][0]);
+    };
+    if (e.key === "Escape") {
+        if( state === "play") {
+            state = "pause";
+        } else {
+            state = "play";
+            ui.pauseState = "none";
+        }
+    }
+    if (e.key === "r") {
+        
+        lastTimestamp = performance.now();
+        sound.play("music", 0);
+        player.pos = {
+            x: 0,//ctx.canvas.width/2 - this.width/2,
+            y: 0//ctx.canvas.height/2 - this.height/2
+        }
+        map.setlevel("level1");
+        requestAnimationFrame(gameLoop);
+        this.disabled = true;
+    }
+    if(e.key === "0") {
+        map.setlevel("level");
+    }
+    if(e.key === "1") {
+        map.setlevel("level1");
+    }
+    map.keyDownInput(e.key)
+    player.keyDownInput(e.key);
+    ui.keyDownInput(e.key);
+});
+
+canvas.addEventListener('keyup', function(e) {
+    e.preventDefault();
+    player.keyUpInput(e.key);
+    for(let i = 0; i < enemies.length; i++) {
+        if(enemies[i].projectileEnemy) {
+            enemies[i].keyUpInput(e.key);}
+        }
+});
+
+canvas.addEventListener('click', function(e) {
+    e.preventDefault();
+    ui.onClickInput(e);
+    if(ui.pauseState == "change to play") {
+        state = "play";
+    }
+}, false);
+
